df400b82b12e2c4435221efcfc2e999801a06856
Cintia_Dourado <cdouradom@gmail.com>
2025-09-18 23:53:47 -0300

    Atividade do modulo 9.2

---
+++ atividades_java/src/atividades_9_2/Fila_Queue.java
@@ -0,0 +1,71 @@
+package atividades_9_2;
+
+import java.util.LinkedList; //biblioteca para a classe de Queue
+import java.util.Queue; //biblioteca para a classe Queue
+import java.util.Scanner; //biblioteca para a classe de ler do teclado
+
+public class Fila_Queue {
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+		
+		//criando a fila de clientes com regra fifo, onde o primeiro que entra é o primeiro que sai)
+		Queue<String> fila = new LinkedList<>(); 
+
+		int opcao; //variavel para armazenar a escolha do menu
+		String nome; //nome do cliente a ser adicionado
+
+		do {
+			//mostrando o menu de opcoes
+			System.out.println("\n========= MENU FILA ==========");
+			System.out.println("1 - Adicionar cliente na fila   ");
+			System.out.println("2 - Listar todos os clientes    ");
+			System.out.println("3 - Retirar cliente da fila	    ");
+			System.out.println("0 - Sair                        ");
+			System.out.println("\n==============================");
+			System.out.print("Digite uma opção do menu: ");
+
+			opcao = leia.nextInt();
+			leia.nextLine();
+
+			switch (opcao) {
+			case 1: //adicionado clientes
+				System.out.print("Digite o nome do Cliente: ");
+				nome = leia.nextLine(); //le o nome do cliente
+				fila.add(nome); //adiciona na fila
+				System.out.println("\nCliente adicionado!");
+				break;
+
+			case 2: //listando os clientes
+				System.out.println("\nLista de Clientes na Fila:");
+				if (fila.isEmpty()) {
+					System.out.println("A fila esta vazia!");
+				} else {
+					for (String cliente : fila) {
+						System.out.println(cliente);
+					}
+				}
+				break;
+
+			case 3: //chama os cliente
+				if (fila.isEmpty()) {
+					System.out.println("\nA Fila esta vazia!");
+				} else {
+					String chamado = fila.poll(); //remove o primeiro da fila
+					System.out.println("\nO Cliente " + chamado + " foi chamado!");
+				}
+				break;
+
+			case 0: //sair
+				System.out.println("\nPrograma Finalizado!");
+				break;
+
+			default:
+				System.out.println("\nOpção invalida, tente novamente.");
+			}
+
+		} while (opcao != 0); //repete o laco ate o usuario escolher sair
+
+		leia.close();
+	}
+}
---
+++ atividades_java/src/atividades_9_2/Fila_Stack.java
@@ -0,0 +1,72 @@
+package atividades_9_2;
+
+import java.util.Scanner; //biblioteca para a classe de ler do teclado
+import java.util.Stack; //biblioteca para a classe de pilha
+
+public class Fila_Stack {
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		// criando a fila de clientes com regra lifo, onde o ultimo que entra eh o
+		// primeiro que sai)
+		Stack<String> pilha = new Stack<>();
+
+		int opcao; // variavel para armazenar a escolha do menu
+		String livro; // nome do cliente a ser adicionado
+
+		do {
+			// mostrando o menu de opcoes
+
+			System.out.println("\n========= MENU PILHA ==========");
+			System.out.println("1 - Adicionar LIVRO              ");
+			System.out.println("2 - Listar todos os livros       ");
+			System.out.println("3 - Retirar livro         	     ");
+			System.out.println("0 - Sair                         ");
+			System.out.println("\n===============================");
+			System.out.print("Digite uma opção do menu:          ");
+
+			opcao = leia.nextInt();
+			leia.nextLine();
+
+			switch (opcao) {
+			case 1: // adicionado os livros
+				System.out.print("Digite o nome do Livro: ");
+				livro = leia.nextLine();
+				pilha.push(livro); // aqui add o livro no topo da pilha
+				System.out.println("\nLivro adicionado!");
+				break;
+
+			case 2: // listando os livros
+				System.out.println("\nLista de Livros na Pilha:");
+				if (pilha.isEmpty()) {
+					System.out.println("A Pilha esta vazia!");
+				} else {
+					for (String l : pilha) {
+						System.out.println(l);
+					}
+				}
+				break;
+
+			case 3: // retirando o livro
+				if (pilha.isEmpty()) {
+					System.out.println("\nA Pilha esta vazia!");
+				} else {
+					String retirado = pilha.pop(); // remove o ultimo livro adicionado, ou seja, do topo
+					System.out.println("\nO Livro \"" + retirado + "\" foi retirado da Pilha!");
+				}
+				break;
+
+			case 0: // sair
+				System.out.println("\nPrograma encerrado!");
+				break;
+
+			default:
+				System.out.println("\nOpcao invalida, tente novamente.");
+			}
+
+		} while (opcao != 0); // repete o laco ate o usuario escolher sair
+
+		leia.close();
+	}
+}
