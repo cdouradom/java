493fb8bfd8730c994224b68f90a44bd168ee4c93
Cintia_Dourado <cdouradom@gmail.com>
2025-09-21 18:44:35 -0300

    Correcoes de pastas

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// println printa e pula linha - se usar só print td vai ficar na mesma linha
+
+		System.out.println("Hello World!");
+
+	}
+
+}
