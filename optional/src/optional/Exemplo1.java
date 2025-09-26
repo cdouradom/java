4f5ea3ee7b67666b4eeb68f26fdc95afbbba11a1
Cintia_Dourado <cdouradom@gmail.com>
2025-09-26 10:00:26 -0300

    Subindo options

---
+++ optional/src/optional/Exemplo1.java
@@ -0,0 +1,20 @@
+package optional;
+
+import java.util.Optional;
+
+public class Exemplo1 {
+
+	public static void main(String[] args) {
+		
+		String[] palavras = new String[10];
+		
+		Optional<String> checaNulo = Optional.ofNullable(palavras[5]);
+		
+		if(checaNulo.isPresent()) {
+			String palavra = palavras[5].toLowerCase();
+			System.out.println(palavra);
+		}else {
+			System.out.println("O valor eh nulo!");
+		}
+	}
+}
